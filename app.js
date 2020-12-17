460137e4dd33fed7b968c13d95dc6c9e5a11a783
samshaker1 <s.shaker87@gmail.com>
2020-12-17 21:20:53 +0000

    sw update

--- app.js
+++ app.js
@@ -69,4 +69,17 @@ class PWAConfApp {
 }
 window.addEventListener('load', e => {
   new PWAConfApp();
+    registerSW();
 });
+
+async function registerSW() { 
+  if ('serviceWorker' in navigator) { 
+    try {
+      await navigator.serviceWorker.register('./sw.js'); 
+    } catch (e) {
+      alert('ServiceWorker registration failed. Sorry about that.'); 
+    }
+  } else {
+    document.querySelector('.alert').removeAttribute('hidden'); 
+  }
+}
